dda4f3f0d7b77ba23b3a26159969e2f71796a01e
Shijie Na <shijiena@connect.hku.hk>
2023-01-05 18:27:34 +0800

    ucb

--- src/policy.py
+++ src/policy.py
@@ -307,12 +307,13 @@ def momentum_select_clients(num_of_client, task_list):
         ### momemtum_based_grad_proj 是一个list，长度等于 总的client数量，挑出momemtum_based_grad_proj最小的num_users client
         # 这里client_state 不需要传参了， 因为client_state在这个函数定义之前就已经定义了，函数内部可以直接访问client_state ok？
         momemtum_based_grad_proj = _task.client_state.client2proj
-        #print(momemtum_based_grad_proj)
+        print(momemtum_based_grad_proj)
         assert isinstance(momemtum_based_grad_proj, list) or isinstance(momemtum_based_grad_proj, np.ndarray)
         assert len(momemtum_based_grad_proj) == num_of_client
 
         momemtum_based_grad_proj = np.array(momemtum_based_grad_proj)
-        sorted_client_idxs = momemtum_based_grad_proj.argsort()[::-1]
+        ucb= momemtum_based_grad_proj + np.sqrt((2* np.log(_task.cient_update_cnt))/_task.client_state.client2selected_cnt)
+        sorted_client_idxs = ucb.argsort()[::-1]
 
         ### Select clients
         selected_client_index = []
