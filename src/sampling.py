865c24cd729211482efb12091d37bc0f5dc28fd0
Hanpeng Hu <joapolarbear@gmail.com>
2023-02-13 15:13:28 +0800

    Load cifar dataset and partition it

--- src/sampling.py
+++ src/sampling.py
@@ -10,7 +10,9 @@ from collections import Counter
 def check_dist(name, _dataset):
     _, lables = zip(*list((_dataset)))
     lables = [int(x) for x in lables]
-    print(f"{name}, distribution: {Counter(lables)}")
+    counter = Counter(lables)
+    print(f"{name}, distribution: {counter}")
+    return counter
 
 def mnist_iid(dataset, num_users):
     """
@@ -20,12 +22,12 @@ def mnist_iid(dataset, num_users):
     :return: dict of image index
     """
     num_items = int(len(dataset)/num_users)
-    dict_users, all_idxs = {}, [i for i in range(len(dataset))]
+    client2dataidxs, all_idxs = {}, [i for i in range(len(dataset))]
     for i in range(num_users):
-        dict_users[i] = set(np.random.choice(all_idxs, num_items,
+        client2dataidxs[i] = set(np.random.choice(all_idxs, num_items,
                                              replace=False))
-        all_idxs = list(set(all_idxs) - dict_users[i])
-    return dict_users
+        all_idxs = list(set(all_idxs) - client2dataidxs[i])
+    return client2dataidxs
 
 
 def mnist_noniid_v1(dataset, num_users):
@@ -38,7 +40,7 @@ def mnist_noniid_v1(dataset, num_users):
     # 60,000 training imgs -->  200 imgs/shard X 300 shards
     num_shards, num_imgs = 200, 300
     idx_shard = [i for i in range(num_shards)]
-    dict_users = {i: np.array([]) for i in range(num_users)}
+    client2dataidxs = {i: np.array([]) for i in range(num_users)}
     idxs = np.arange(num_shards*num_imgs)
     labels = dataset.train_labels.numpy()
 
@@ -52,9 +54,9 @@ def mnist_noniid_v1(dataset, num_users):
         rand_set = set(np.random.choice(idx_shard, 2, replace=False))
         idx_shard = list(set(idx_shard) - rand_set)
         for rand in rand_set:
-            dict_users[i] = np.concatenate(
-                (dict_users[i], idxs[rand*num_imgs:(rand+1)*num_imgs]), axis=0)
-    return dict_users
+            client2dataidxs[i] = np.concatenate(
+                (client2dataidxs[i], idxs[rand*num_imgs:(rand+1)*num_imgs]), axis=0)
+    return client2dataidxs
 
 def mnist_noniid_v2(dataset, num_users):
     """
@@ -64,7 +66,7 @@ def mnist_noniid_v2(dataset, num_users):
     :return:
     """
     # 60,000 training imgs -->  200 imgs/shard X 300 shards
-    dict_users = {i: np.array([]) for i in range(num_users)}
+    client2dataidxs = {i: np.array([]) for i in range(num_users)}
     # idxs = np.arange(len(dataset))
     labels = dataset.train_labels.numpy()
 
@@ -105,16 +107,16 @@ def mnist_noniid_v2(dataset, num_users):
         for _class in range(CLASS_NUM):
             sample_idxs_of_this_class = label2idxs[_class]
             if _class in major_class:
-                dict_users[i] = np.concatenate((dict_users[i],
+                client2dataidxs[i] = np.concatenate((client2dataidxs[i],
                     np.random.choice(sample_idxs_of_this_class, sample_num_per_major_class, replace=False)), axis=0)
             else:
                 ### In minor class
-                dict_users[i] = np.concatenate((dict_users[i],
+                client2dataidxs[i] = np.concatenate((client2dataidxs[i],
                     np.random.choice(sample_idxs_of_this_class, sample_num_per_minor_class, replace=False)), axis=0)
 
-    return dict_users
+    return client2dataidxs
 
-CLIENT_DATA_DIST = [
+CLIENT_DATA_DIST = np.array([
     #  0,  1,  2,   3,   4,   5,  6,   7,    8,  9
     [1.4, 1.4, 30, 1.4, 1.4, 30, 1.4, 1.4, 1.6, 30],#client0
     [1.4, 1.4, 1.4, 30, 1.4, 1.4, 30, 1.4, 1.6, 30],
@@ -126,35 +128,44 @@ CLIENT_DATA_DIST = [
     [30, 1.4, 1.4, 30, 1.4, 1.4, 1.4, 30, 1.4, 1.6],
     [1.4, 1.4, 1.4, 1.4, 1.4, 30, 30, 1.4, 30, 1.6],
     [30 ,  30, 1.4,1.4,  30, 1.4, 1.4,1.4,1.4, 1.6]
-]
+])
 
-CLIENT_DATA_NUM = [1200*5] * 10
-CLASS_NUM = 10
+CLIENT_NUM = CLIENT_DATA_DIST.shape[0]
+CLASS_NUM = CLIENT_DATA_DIST.shape[1]
 
-def mnist_noniid_v3(dataset, num_users):
+def load_custom_dataset(dataset, num_users):
     """
     Sample non-I.I.D client data from MNIST dataset
     :param dataset:
     :param num_users:
     :return:
     """
+
+    counter = check_dist("Origin Dataset", dataset)
+
+    total_data_size = sum(counter.values())
+    data_size_per_client = int(total_data_size / 10)
+
+    ### Assume ach client has the same size of data
+    CLIENT_DATA_NUM = [data_size_per_client] * 10
+
     # 60,000 training imgs -->  200 imgs/shard X 300 shards
-    dict_users: Dict[int, np.ndarray] = {i: np.array([]) for i in range(num_users)}
-    # idxs = np.arange(len(dataset))
-    labels = dataset.train_labels.numpy()
+    client2dataidxs: Dict[int, np.ndarray] = {i: np.array([]) for i in range(num_users)}
 
     # Group labels
-    label2idxs = []
-    for digit in range(CLASS_NUM):
-        indexs = [i for i, label in enumerate(labels) if label == digit]
-        # indexs = indexs[0:5421]
-        label2idxs.append(indexs)
+    label2idxs = {}
+    for idx, (_, y) in enumerate(dataset):
+        if y not in label2idxs:
+            label2idxs[y] = []
+        label2idxs[y].append(idx)
 
+    ### When traversing all data by label, store the current 
+    # position of the idx list of each label
     label2bias = [0] * CLASS_NUM
     
     for client_id in range(num_users):
 
-        _dist: np.ndarray = np.array(CLIENT_DATA_DIST[client_id])
+        _dist: np.ndarray = CLIENT_DATA_DIST[client_id]
         _data_num: int = CLIENT_DATA_NUM[client_id]
         _data_num_per_label: np.ndarray = _dist * _data_num / 100
 
@@ -163,9 +174,9 @@ def mnist_noniid_v3(dataset, num_users):
             required_data_num_this_class: int = int(_data_num_per_label[_class_id])
             required_data_idxs = all_data_idxs_of_this_class[label2bias[_class_id]:(label2bias[_class_id]+required_data_num_this_class)]
             label2bias[_class_id] += required_data_num_this_class
-            dict_users[client_id] = np.concatenate((dict_users[client_id], required_data_idxs), axis=0)
+            client2dataidxs[client_id] = np.concatenate((client2dataidxs[client_id], required_data_idxs), axis=0)
 
-    return dict_users
+    return client2dataidxs
 
 def mnist_noniid(dataset, num_users):
     # return mnist_noniid_v1(dataset, num_users)
@@ -174,14 +185,13 @@ def mnist_noniid(dataset, num_users):
 def mnist_iid_noniid(dataset, num_users):
     dict_iid = mnist_iid(dataset, num_users)
     dict_non_iid = mnist_noniid(dataset, num_users)
-    dict_users = {i: np.array([]) for i in range(num_users)}
+    client2dataidxs = {i: np.array([]) for i in range(num_users)}
     for i in range(num_users):
         if i < num_users/2:
-            dict_users[i] = dict_iid[i]
+            client2dataidxs[i] = dict_iid[i]
         else:
-            dict_users[i] = dict_non_iid[i]
-    return dict_users
-
+            client2dataidxs[i] = dict_non_iid[i]
+    return client2dataidxs
 
 def mnist_noniid_unequal(dataset, num_users):
     """
@@ -195,7 +205,7 @@ def mnist_noniid_unequal(dataset, num_users):
     # 60,000 training imgs --> 50 imgs/shard X 1200 shards
     num_shards, num_imgs = 1200, 50
     idx_shard = [i for i in range(num_shards)]
-    dict_users = {i: np.array([]) for i in range(num_users)}
+    client2dataidxs = {i: np.array([]) for i in range(num_users)}
     idxs = np.arange(num_shards*num_imgs)
     labels = dataset.train_labels.numpy()
 
@@ -225,8 +235,8 @@ def mnist_noniid_unequal(dataset, num_users):
             rand_set = set(np.random.choice(idx_shard, 1, replace=False))
             idx_shard = list(set(idx_shard) - rand_set)
             for rand in rand_set:
-                dict_users[i] = np.concatenate(
-                    (dict_users[i], idxs[rand*num_imgs:(rand+1)*num_imgs]),
+                client2dataidxs[i] = np.concatenate(
+                    (client2dataidxs[i], idxs[rand*num_imgs:(rand+1)*num_imgs]),
                     axis=0)
 
         random_shard_size = random_shard_size-1
@@ -242,8 +252,8 @@ def mnist_noniid_unequal(dataset, num_users):
                                             replace=False))
             idx_shard = list(set(idx_shard) - rand_set)
             for rand in rand_set:
-                dict_users[i] = np.concatenate(
-                    (dict_users[i], idxs[rand*num_imgs:(rand+1)*num_imgs]),
+                client2dataidxs[i] = np.concatenate(
+                    (client2dataidxs[i], idxs[rand*num_imgs:(rand+1)*num_imgs]),
                     axis=0)
     else:
 
@@ -253,24 +263,24 @@ def mnist_noniid_unequal(dataset, num_users):
                                             replace=False))
             idx_shard = list(set(idx_shard) - rand_set)
             for rand in rand_set:
-                dict_users[i] = np.concatenate(
-                    (dict_users[i], idxs[rand*num_imgs:(rand+1)*num_imgs]),
+                client2dataidxs[i] = np.concatenate(
+                    (client2dataidxs[i], idxs[rand*num_imgs:(rand+1)*num_imgs]),
                     axis=0)
 
         if len(idx_shard) > 0:
             # Add the leftover shards to the client with minimum images:
             shard_size = len(idx_shard)
             # Add the remaining shard to the client with lowest data
-            k = min(dict_users, key=lambda x: len(dict_users.get(x)))
+            k = min(client2dataidxs, key=lambda x: len(client2dataidxs.get(x)))
             rand_set = set(np.random.choice(idx_shard, shard_size,
                                             replace=False))
             idx_shard = list(set(idx_shard) - rand_set)
             for rand in rand_set:
-                dict_users[k] = np.concatenate(
-                    (dict_users[k], idxs[rand*num_imgs:(rand+1)*num_imgs]),
+                client2dataidxs[k] = np.concatenate(
+                    (client2dataidxs[k], idxs[rand*num_imgs:(rand+1)*num_imgs]),
                     axis=0)
 
-    return dict_users
+    return client2dataidxs
 
 
 def cifar_iid(dataset, num_users):
@@ -281,13 +291,12 @@ def cifar_iid(dataset, num_users):
     :return: dict of image index
     """
     num_items = int(len(dataset)/num_users)
-    dict_users, all_idxs = {}, [i for i in range(len(dataset))]
+    client2dataidxs, all_idxs = {}, [i for i in range(len(dataset))]
     for i in range(num_users):
-        dict_users[i] = set(np.random.choice(all_idxs, num_items,
+        client2dataidxs[i] = set(np.random.choice(all_idxs, num_items,
                                              replace=False))
-        all_idxs = list(set(all_idxs) - dict_users[i])
-    return dict_users
-
+        all_idxs = list(set(all_idxs) - client2dataidxs[i])
+    return client2dataidxs
 
 def cifar_noniid(dataset, num_users):
     """
@@ -298,7 +307,7 @@ def cifar_noniid(dataset, num_users):
     """
     num_shards, num_imgs = 200, 250
     idx_shard = [i for i in range(num_shards)]
-    dict_users = {i: np.array([]) for i in range(num_users)}
+    client2dataidxs = {i: np.array([]) for i in range(num_users)}
     idxs = np.arange(num_shards*num_imgs)
     # labels = dataset.train_labels.numpy()
     labels = np.array(dataset.targets)
@@ -313,9 +322,9 @@ def cifar_noniid(dataset, num_users):
         rand_set = set(np.random.choice(idx_shard, 2, replace=False))
         idx_shard = list(set(idx_shard) - rand_set)
         for rand in rand_set:
-            dict_users[i] = np.concatenate(
-                (dict_users[i], idxs[rand*num_imgs:(rand+1)*num_imgs]), axis=0)
-    return dict_users
+            client2dataidxs[i] = np.concatenate(
+                (client2dataidxs[i], idxs[rand*num_imgs:(rand+1)*num_imgs]), axis=0)
+    return client2dataidxs
 
 
 def get_dataset(args):
@@ -336,13 +345,15 @@ def get_dataset(args):
         test_dataset = datasets.CIFAR10(data_dir, train=False, download=True,
                                       transform=apply_transform)
 
-        check_dist("Cifar Train", train_dataset)
-        check_dist("Cifar Test", test_dataset)           
+        # check_dist("Cifar Train", train_dataset)
+        # check_dist("Cifar Test", test_dataset)           
 
         # sample training data amongst users
-        if args.iid:
+        if True:
+            client2dataidxs = load_custom_dataset(dataset=train_dataset, num_users=args.num_users)
+        elif args.iid:
             # Sample IID user data from Mnist
-            user_groups = cifar_iid(train_dataset, args.num_users)
+            client2dataidxs = cifar_iid(train_dataset, args.num_users)
         else:
             # Sample Non-IID user data from Mnist
             if args.unequal:
@@ -350,7 +361,7 @@ def get_dataset(args):
                 raise NotImplementedError()
             else:
                 # Chose euqal splits for every user
-                user_groups = cifar_noniid(train_dataset, args.num_users)
+                client2dataidxs = cifar_noniid(train_dataset, args.num_users)
 
     elif args.dataset == 'mnist' or 'fmnist':
         if args.dataset == 'mnist':
@@ -368,25 +379,24 @@ def get_dataset(args):
         test_dataset = datasets.MNIST(data_dir, train=False, download=True,
                                       transform=apply_transform)
 
-
         # sample training data amongst users
         if True:
-            user_groups = mnist_noniid_v3(dataset=train_dataset, num_users=args.num_users)
+            client2dataidxs = load_custom_dataset(dataset=train_dataset, num_users=args.num_users)
         elif args.halfiid:
-            user_groups = mnist_iid_noniid(train_dataset, args.num_users)
+            client2dataidxs = mnist_iid_noniid(train_dataset, args.num_users)
         elif args.iid:
             # Sample IID user data from Mnist
-            user_groups = mnist_iid(train_dataset, args.num_users)
+            client2dataidxs = mnist_iid(train_dataset, args.num_users)
         else:
             # Sample Non-IID user data from Mnist
             if args.unequal:
                 # Chose uneuqal splits for every user
-                user_groups = mnist_noniid_unequal(train_dataset, args.num_users)
+                client2dataidxs = mnist_noniid_unequal(train_dataset, args.num_users)
             else:
                 # Chose euqal splits for every user
-                user_groups = mnist_noniid(train_dataset, args.num_users)
+                client2dataidxs = mnist_noniid(train_dataset, args.num_users)
 
-    return train_dataset, test_dataset, user_groups
+    return train_dataset, test_dataset, client2dataidxs
 
 
 if __name__ == '__main__':
