7ea44e3ffc36b0bb27f326c28028c2ab654702ea
Hanpeng Hu <joapolarbear@gmail.com>
2023-12-17 17:32:59 +0800

    plot figure

--- plot/exp1.py
+++ plot/exp1.py
@@ -37,23 +37,33 @@ exp_dir = sys.argv[1]
 
 ALL_DF = {}
 
-
 def key_to_config(key: str):
     '''convert a serialized key to a dict of configuration'''
     cfg = re.search(f"(?P<dataset>\w+)-(?P<target_label>\w+)_label-(?P<model>\w+)-(?P<policy>\w+)_policy", key)
     return cfg
 
-def policy_legend_name(policy):
-    if policy.lower() == "nmfli":
-        return "NmFLI"
-    elif policy.lower() == "size":
-        return "Accord2Size"
-    elif policy.lower() in ["greedy", "random", "simple"]:
-        return policy[0].upper() + policy[1:].lower()
-    elif policy.lower() in ["afl"]:
-        return policy.upper()
-    else:
-        raise ValueError(policy)
+POLICY_TO_INFO = {
+    "nmfli": ("NmFLI", "red", "-"),
+    "size": ("Data size-based", "orange", ":"),
+    "greedy": ("Greedy", "purple", (0, (3, 5, 1, 5, 1, 5))),
+    "random": ("Random", "green", "--"),
+    "afl": ("AFL", "blue", "-."),
+    "simple": ("Simple", "black", (0, (3, 1, 1, 1, 1, 1)))
+}
+
+def policy_to_plot_kwargs(policy):
+    ''' generate legend name, color
+    '''
+    legend_name, color, linestyle = POLICY_TO_INFO[policy.lower()]
+    kwargs = {
+        "label": legend_name,
+        "color": color,
+        "linestyle": linestyle
+    }
+    if policy == "nmfli":
+        kwargs["linewidth"] = 2
+        # kwargs["markersize"] = 2
+    return kwargs
 
 IGNORE_POLICY = ["simple"]
 target_label_to_fig_dict = {}
@@ -65,7 +75,7 @@ for _file in sorted(os.listdir(exp_dir)):
     rst = key_to_config(_file)
     if rst["policy"] in IGNORE_POLICY:
         continue
-    print(rst["dataset"], rst["target_label"], rst["model"], rst["policy"])
+    # print(rst["dataset"], rst["target_label"], rst["model"], rst["policy"])
     if rst["target_label"] not in target_label_to_fig_dict:
         target_label_to_fig_dict[rst["target_label"]] = [[] for _ in range(sub_fig_num)]
         
@@ -74,38 +84,54 @@ for _file in sorted(os.listdir(exp_dir)):
     sub_fig_list = target_label_to_fig_dict[rst["target_label"]]
     # ax0.plot(df["Step"], df["Task 0 test accu."], alpha=0.3)
     # smooth
-    kwargs = {
-        "label": policy_legend_name(rst["policy"])
-    }
-    if rst["policy"] == "nmfli":
-        kwargs["color"] = "red"
-        kwargs["linewidth"] = 2
-        # kwargs["markersize"] = 2
+    kwargs = policy_to_plot_kwargs(rst["policy"])
     sub_fig_list[0].append((df["Step"], smooth(df["Task 0 test accu."], .9), kwargs))
     sub_fig_list[1].append((df["Step"], smooth(df["Task 1 test accu."], .9), kwargs))
     sub_fig_list[2].append((df["Step"], (smooth(df["Task 0 test accu."], .9) + smooth(df["Task 1 test accu."], .9)) / 2, kwargs))
 
-for target_label in target_label_to_fig_dict.keys():
-    sub_fig_list = target_label_to_fig_dict[target_label]
-    fig = plt.figure(figsize=(12, 8))
-    _fig_base = fig_base(sub_fig_num)
-    all_subfig = []
-    for sub_fig_id in range(sub_fig_num):
-        ax = fig.add_subplot(_fig_base + sub_fig_id + 1); ax.grid()
-        all_subfig.append(ax)
+is_one_figure = False
 
-    for sub_fig_id, sub_fig_data in enumerate(sub_fig_list):
-        for x, y, kwargs in sub_fig_data:
-            all_subfig[sub_fig_id].plot(x, y, **kwargs)
-            
-    for sub_fig_id in range(sub_fig_num):
-        all_subfig[sub_fig_id].set_xlabel("Step")
-        all_subfig[sub_fig_id].set_ylabel("Task 0 test accu. (smoothed)")
-        all_subfig[sub_fig_id].legend()
+sub_figure_y_labels = [
+    "Task 0 test accu. (smoothed)",
+    "Task 1 test accu. (smoothed)",
+    "Average test accu. (smoothed)"
+]
+if is_one_figure:
+    for target_label in target_label_to_fig_dict.keys():
+        sub_fig_list = target_label_to_fig_dict[target_label]
+        fig = plt.figure(figsize=(12, 8))
+        _fig_base = fig_base(sub_fig_num)
+        all_subfig = []
+        for sub_fig_id in range(sub_fig_num):
+            ax = fig.add_subplot(_fig_base + sub_fig_id + 1); ax.grid()
+            all_subfig.append(ax)
 
-    plt.tight_layout()
-    plt.savefig(os.path.join(exp_dir, f"{target_label}.pdf"))
-    plt.close()
+        for sub_fig_id, sub_fig_data in enumerate(sub_fig_list):
+            for x, y, kwargs in sub_fig_data:
+                all_subfig[sub_fig_id].plot(x, y, **kwargs)
+                
+        for sub_fig_id in range(sub_fig_num):
+            all_subfig[sub_fig_id].set_xlabel("Step")
+            all_subfig[sub_fig_id].set_ylabel(sub_figure_y_labels[sub_fig_id])
+            all_subfig[sub_fig_id].legend()
+
+        plt.tight_layout()
+        plt.savefig(os.path.join(exp_dir, f"{os.path.basename(exp_dir)}-{target_label}.pdf"))
+        plt.close()
+else:
+    for target_label in target_label_to_fig_dict.keys():
+        sub_fig_list = target_label_to_fig_dict[target_label]
+        for sub_fig_id, sub_fig_data in enumerate(sub_fig_list):
+            fig = plt.figure(figsize=(6, 4))
+            for x, y, kwargs in sub_fig_data:
+                plt.plot(x, y, **kwargs)
+            plt.grid()
+            plt.xlabel("Step")
+            plt.ylabel(sub_figure_y_labels[sub_fig_id])
+            plt.legend()
+            plt.tight_layout()
+            plt.savefig(os.path.join(exp_dir, f"{os.path.basename(exp_dir)}-{target_label}_{sub_fig_id}.pdf"))
+            plt.close()
         
     
 
--- script/plot_all.sh
+++ script/plot_all.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
+set -x
 # NOTE: must be called from root directory of the project: bash script/plot_all.sh
 
-# cifar even dataset
+# Cifar even dataset
+## Non-overlap
 python3 plot/exp1.py save/results/20231205-170051-even_size_custom-gpu-cifar
 
-# 
+## Indentical and overlap 
 python3 plot/exp1.py save/results/20231206-144643-even_size_custom-gpu-cifar
+
+## Others
+python3 plot/exp1.py save/results/20231215-123505-noisy_x
+python3 plot/exp1.py save/results/20231214-040304-req_1_2_client
+python3 plot/exp1.py save/results/20231214-040326-req_3_2_client
+
+
+# Uneven data size
+
+python3 plot/exp1.py save/results/20231216-100943-uneven_datasize
+
+
